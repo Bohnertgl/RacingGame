db6e6e31781f18f1bfc3f6275f4083f59bb45f2f
Bohnertgl <gary.bohnert@users.noreply.github.com>
2021-02-25 15:19:13 -0500

    Add project files.

---
+++ RacingGame/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace RacingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Line1 = new System.Windows.Forms.PictureBox();
+            this.Line2 = new System.Windows.Forms.PictureBox();
+            this.Line3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.car = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Line1
+            // 
+            this.Line1.BackColor = System.Drawing.Color.White;
+            this.Line1.Location = new System.Drawing.Point(189, 1);
+            this.Line1.Name = "Line1";
+            this.Line1.Size = new System.Drawing.Size(10, 100);
+            this.Line1.TabIndex = 0;
+            this.Line1.TabStop = false;
+            // 
+            // Line2
+            // 
+            this.Line2.BackColor = System.Drawing.Color.White;
+            this.Line2.Location = new System.Drawing.Point(189, 165);
+            this.Line2.Name = "Line2";
+            this.Line2.Size = new System.Drawing.Size(10, 100);
+            this.Line2.TabIndex = 1;
+            this.Line2.TabStop = false;
+            // 
+            // Line3
+            // 
+            this.Line3.BackColor = System.Drawing.Color.White;
+            this.Line3.Location = new System.Drawing.Point(189, 333);
+            this.Line3.Name = "Line3";
+            this.Line3.Size = new System.Drawing.Size(10, 100);
+            this.Line3.TabIndex = 2;
+            this.Line3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 460);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(374, 1);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 460);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            // 
+            // car
+            // 
+            this.car.BackColor = System.Drawing.Color.Transparent;
+            this.car.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.car.Image = ((System.Drawing.Image)(resources.GetObject("car.Image")));
+            this.car.Location = new System.Drawing.Point(270, 362);
+            this.car.Name = "car";
+            this.car.Size = new System.Drawing.Size(44, 71);
+            this.car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car.TabIndex = 5;
+            this.car.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.car);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.Line3);
+            this.Controls.Add(this.Line2);
+            this.Controls.Add(this.Line1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Line3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Line1;
+        private System.Windows.Forms.PictureBox Line2;
+        private System.Windows.Forms.PictureBox Line3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox car;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RacingGame/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RacingGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void Line1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void Line2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Line3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void car_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveline(5);
+        }
+        void moveline(int speed)
+        {
+            if (Line1.Top >= 500)
+            {
+                Line1.Top = 0;
+            }
+            else
+            {
+                Line1.Top += speed;
+            }
+
+            if (Line2.Top >= 500)
+            {
+                Line2.Top = 0;
+            }
+            else
+            {
+                Line2.Top += speed;
+            }
+
+            if (Line3.Top >= 500)
+            {
+                Line3.Top = 0;
+            }
+            else
+            {
+                Line3.Top += speed;
+            }
+        }
+        int gamespeed = 0;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (car.Left > 0)
+                    car.Left += -8;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (car.Right < 380)
+                    car.Left += 8;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (gamespeed < 21)
+                { gamespeed++; }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (gamespeed > 0)
+                { gamespeed--; }
+            }
+        }
+    }
+}
